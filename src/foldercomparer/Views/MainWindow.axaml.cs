d2c0b765b8135d473846a0121b422a817102d20f
amanda <amanda.fu.work@gmail.com>
2025-04-24 14:51:00 -0400

    update

--- src/foldercomparer/Views/MainWindow.axaml.cs
+++ src/foldercomparer/Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System.Threading.Tasks;
 
 namespace foldercomparer.Views;
 
@@ -7,5 +9,35 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    public async void SelectFolder1(object? sender, RoutedEventArgs e)
+    {
+        var dialog = new OpenFolderDialog
+        {
+            Title = "Choose a folder",
+        };
+
+        var result = await dialog.ShowAsync(this);
+
+        if (result is { Length: > 0 })
+        {
+            folder1TextBox.Text = result; // Update UI with selected file path
+        }
+    }
+
+    public async void SelectFolder2(object? sender, RoutedEventArgs e)
+    {
+        var dialog = new OpenFolderDialog
+        {
+            Title = "Choose a folder",
+        };
+
+        var result = await dialog.ShowAsync(this);
+
+        if (result is { Length: > 0 })
+        {
+            folder2TextBox.Text = result; // Update UI with selected file path
+        }
     }
 }
