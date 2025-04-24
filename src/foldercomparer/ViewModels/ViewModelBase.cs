36142b7add0711d91984547647f14d5a13a47764
amanda <amanda.fu.work@gmail.com>
2025-04-24 10:47:09 -0400

    avalonia.mvvm

---
+++ src/foldercomparer/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace foldercomparer.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ src/foldercomparer/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace foldercomparer.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
