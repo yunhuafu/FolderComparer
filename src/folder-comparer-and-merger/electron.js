5136d1fb75218120c33cd4eb7b53870e333cd92a
Amanda <amanda.fu.work@gmail.com>
2025-04-26 10:27:34 -0400

    electron for Mac

--- src/folder-comparer-and-merger/electron.js
+++ src/folder-comparer-and-merger/electron.js
@@ -26,16 +26,18 @@ function createWindow() {
   mainWindow.on('closed', () => (mainWindow = null));
 }
 
-app.on('ready', createWindow);
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow();
+    app.quit()
   }
 });
