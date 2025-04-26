541a11d59d636a387ed2789a23073324ab62d4ad
Amanda <amanda.fu.work@gmail.com>
2025-04-25 23:51:11 -0400

    with split panel

--- src/folder-comparer-and-merger/src/App.tsx
+++ src/folder-comparer-and-merger/src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TreeView from './components/treeview/TreeView';
+import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PanelGroup direction="horizontal">
+        <Panel minSize={20}>
+          collapse
+        </Panel>
+        <PanelResizeHandle />
+        <Panel defaultSize={30} minSize={30} className='panel-treeview'>
+          <TreeView/>
+        </Panel>
+        <PanelResizeHandle />
+        <Panel defaultSize={30} minSize={20} className='panel-treeview'>
+          <TreeView/>
+        </Panel>
+      </PanelGroup>
     </div>
   );
 }
---
+++ src/folder-comparer-and-merger/src/components/treeview/TreeView.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TreeView.css';
+
+function TreeView() {
+  return (
+    <div className="TreeView">
+      treeview
+    </div>
+  );
+}
+
+export default TreeView;
