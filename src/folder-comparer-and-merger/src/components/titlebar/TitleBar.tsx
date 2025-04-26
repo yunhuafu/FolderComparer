e69c451c2c18261e68b2b06de72e7ddcbdc4fda5
Amanda <amanda.fu.work@gmail.com>
2025-04-26 10:06:59 -0400

    layout

--- src/folder-comparer-and-merger/src/components/titlebar/TitleBar.tsx
+++ src/folder-comparer-and-merger/src/components/titlebar/TitleBar.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import CompareIcon from '@mui/icons-material/Compare';
+import { blue } from '@mui/material/colors';
+import { TextField, Box, Button, Grid, Stack } from '@mui/material'
 
 function TitleBar() {
   return (
-    <div>
-      <CompareIcon/>
-    </div>
+    <Grid container spacing={2}>
+        <Grid container sx={{justifyContent: "flex-start", alignItems:"center"}}>
+          <CompareIcon sx={{ color: blue[500] }}/>
+        </Grid>
+       
+        <Grid container size="grow" sx={{justifyContent: "flex-end"}}>
+            <Stack direction="row" spacing={2}>
+                <Box component="section" sx={{ p: 2}}/>
+                <TextField variant="outlined"></TextField>
+                <Button variant="contained">Select Folder 1</Button>
+                <Box component="section" sx={{ p: 2}}/>
+                <TextField variant="outlined"></TextField>
+                <Button variant="contained">Select Folder 2</Button>
+                <Box component="section" sx={{ p: 2}}/>
+                <Button variant="contained">Compare</Button>
+                <Box component="section" sx={{ p: 2}}/>
+                <Button variant="contained">Merge</Button>
+            </Stack>
+        </Grid>
+    </Grid>
   );
 }
 
