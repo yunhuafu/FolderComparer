028d622caa3cfa5fccf9b2e683bf185e56c8c74c
Amanda <amanda.fu.work@gmail.com>
2025-04-26 00:16:13 -0400

    add icons

--- src/folder-comparer-and-merger/src/App.tsx
+++ src/folder-comparer-and-merger/src/App.tsx
@@ -3,13 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import TreeView from './components/treeview/TreeView';
 import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
+import KeyboardArrowDownSharpIcon from '@mui/icons-material/KeyboardArrowDownSharp';
+import KeyboardArrowRightSharpIcon from '@mui/icons-material/KeyboardArrowRightSharp';
 
 function App() {
   return (
     <div className="App">
       <PanelGroup direction="horizontal">
-        <Panel minSize={20}>
-          collapse
+        <Panel minSize={30} maxSize={30}>
+          <KeyboardArrowRightSharpIcon/> 
+          <KeyboardArrowDownSharpIcon/> 
         </Panel>
         <PanelResizeHandle />
         <Panel defaultSize={30} minSize={30} className='panel-treeview'>
