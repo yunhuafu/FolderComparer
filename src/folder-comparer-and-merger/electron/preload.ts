0f511b923e692c3ee12e184b1cccec51dfaf28ba
Amanda <amanda.fu.work@gmail.com>
2025-05-02 10:28:25 -0400

    deleted older source

--- src/folder-comparer-and-merger/electron/preload.ts
+++
@@ -1,6 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  selectFolder: () => ipcRenderer.invoke('select-folder'),
-  compareFolders: (folderPath1:string, folderPath2:string) => ipcRenderer.invoke('compare-folders', folderPath1, folderPath2)
-});
