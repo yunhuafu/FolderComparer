2bd90dbeda6528a829e501a3229ce997cdcd0afa
Amanda <amanda.fu.work@gmail.com>
2025-05-07 10:13:18 -0400

    added ComparisonResultItem.ts

---
+++ src/folder-comparer-and-merger/src/models/ComparisonResultItem.ts
@@ -0,0 +1,9 @@
+class ComparisonResultItem {
+  public name: string = ''
+  public path: string = ''
+  public size: number = 0
+  public isFolder: boolean = true
+  public level: number = 0
+}
+
+export default ComparisonResultItem
