025ed43dd98d8dfa513fbf29f5c5441cbe69c5a6
Amanda <amanda.fu.work@gmail.com>
2025-05-13 15:06:00 -0400

    added some background color

--- src/folder-comparer-and-merger/src/models/TraverseResultItem.ts
+++ src/folder-comparer-and-merger/src/models/TraverseResultItem.ts
@@ -1,10 +1,18 @@
 import { DirentForIPC } from './ComparisonResult'
 
+export const ComparisonType = Object.freeze({
+  SAME: 'same',
+  DIFFERENT: 'different',
+  LEFT_ONLY: 'leftOnly',
+  RIGHT_ONLY: 'rightOnly'
+})
+
 class TraverseResultItem {
   constructor(
     public entry1: DirentForIPC | null,
     public entry2: DirentForIPC | null,
-    public level: number
+    public level: number,
+    public type: string
   ) {}
 }
 
--- src/folder-comparer-and-merger/src/renderer/src/components/treeView/TreeView.tsx
+++ src/folder-comparer-and-merger/src/renderer/src/components/treeView/TreeView.tsx
@@ -10,7 +10,7 @@ import { blue, yellow } from '@mui/material/colors'
 import KeyboardArrowDownSharpIcon from '@mui/icons-material/KeyboardArrowDownSharp'
 import KeyboardArrowRightSharpIcon from '@mui/icons-material/KeyboardArrowRightSharp'
 import ComparisonResult from 'src/models/ComparisonResult'
-import TraverseResultItem from 'src/models/TraverseResultItem'
+import TraverseResultItem, { ComparisonType } from 'src/models/TraverseResultItem'
 
 function traverse(
   comparisonResult: ComparisonResult | null,
@@ -21,7 +21,19 @@ function traverse(
     const item: TraverseResultItem = new TraverseResultItem(
       comparisonResultItem[0],
       comparisonResultItem[1],
-      level
+      level,
+      ComparisonType.SAME
+    )
+    traverseResult.push(item)
+    if (comparisonResultItem[0].isDirectory)
+      traverse(comparisonResultItem[2], traverseResult, level + 1)
+  })
+  comparisonResult?.differentEntries.forEach((comparisonResultItem) => {
+    const item: TraverseResultItem = new TraverseResultItem(
+      comparisonResultItem[0],
+      comparisonResultItem[1],
+      level,
+      ComparisonType.DIFFERENT
     )
     traverseResult.push(item)
     if (comparisonResultItem[0].isDirectory)
@@ -34,26 +46,44 @@ const TreeView = forwardRef<HTMLDivElement, CustomComponentProps>((props, ref) =
   const traverseResult: TraverseResultItem[] = []
   traverse(comparisonResult, traverseResult, 0)
 
-  const treeNodes = traverseResult.map((treeNode) => (
-    <li
-      key={treeNode.entry1?.name}
-      style={{ paddingLeft: `${treeNode.level * 20}px` }} // 20px per level
-    >
-      {treeNode.entry1?.isDirectory && (
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <KeyboardArrowDownSharpIcon />
-          <FolderIcon sx={{ color: yellow[500] }}></FolderIcon>
-          <span>{treeNode.entry1?.name}</span>
-        </div>
-      )}
-      {!treeNode.entry1?.isDirectory && (
-        <div style={{ paddingLeft: '20px', display: 'flex', alignItems: 'center' }}>
-          <InsertDriveFileIcon sx={{ color: blue[500] }}></InsertDriveFileIcon>
-          <span>{treeNode.entry1?.name}</span>
-        </div>
-      )}
-    </li>
-  ))
+  const treeNodes = traverseResult.map((treeNode) => {
+    let backgroundColor = 'pink'
+    switch (treeNode.type) {
+      case ComparisonType.SAME:
+        backgroundColor = 'PaleGreen'
+        break
+      case ComparisonType.DIFFERENT:
+        backgroundColor = 'Moccasin'
+        break
+      case ComparisonType.LEFT_ONLY:
+        backgroundColor = 'yellow'
+        break
+      case ComparisonType.RIGHT_ONLY:
+        backgroundColor = 'yellow'
+        break
+    }
+
+    return (
+      <li
+        key={treeNode.entry1?.name}
+        style={{ paddingLeft: `${treeNode.level * 20}px`, backgroundColor: `${backgroundColor}` }} // 20px per level
+      >
+        {treeNode.entry1?.isDirectory && (
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <KeyboardArrowDownSharpIcon />
+            <FolderIcon sx={{ color: yellow[500] }}></FolderIcon>
+            <span>{treeNode.entry1?.name}</span>
+          </div>
+        )}
+        {!treeNode.entry1?.isDirectory && (
+          <div style={{ paddingLeft: '20px', display: 'flex', alignItems: 'center' }}>
+            <InsertDriveFileIcon sx={{ color: blue[500] }}></InsertDriveFileIcon>
+            <span>{treeNode.entry1?.name}</span>
+          </div>
+        )}
+      </li>
+    )
+  })
 
   return (
     <Box ref={ref} {...props}>
