dbc8cfe3b4225061f217de5ffeaeab17ec9d66f3
Amanda <amanda.fu.work@gmail.com>
2025-05-26 00:35:00 -0400

    show folder structure after selecting folder

--- src/folder-comparer-and-merger/src/renderer/src/components/treeViewAfterFolderSelection/TreeViewAfterFolderSelection.tsx
+++ src/folder-comparer-and-merger/src/renderer/src/components/treeViewAfterFolderSelection/TreeViewAfterFolderSelection.tsx
@@ -6,6 +6,11 @@ import { useSelector } from 'react-redux'
 import { selectFileSystemNode1 } from '@renderer/app/fileSystemNode1Slice'
 import { selectFileSystemNode2 } from '@renderer/app/fileSystemNode2Slice'
 import { FileSystemNode } from 'src/models/FileSystemNode'
+import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile'
+import KeyboardArrowDownSharpIcon from '@mui/icons-material/KeyboardArrowDownSharp'
+import KeyboardArrowRightSharpIcon from '@mui/icons-material/KeyboardArrowRightSharp'
+import FolderIcon from '@mui/icons-material/Folder'
+import { yellow } from '@mui/material/colors'
 
 type TreeViewProps = CustomComponentProps & {
   isLeft: boolean
@@ -57,6 +62,52 @@ function getFileSystemViewNode(fileSystemNode: FileSystemNode): FileSystemViewNo
   }
 }
 
+function getTreeNode(fileSystemViewNode: FileSystemViewNode, level: number): React.JSX.Element {
+  if (fileSystemViewNode.isDirectory == false) {
+    return (
+      <div
+        key={fileSystemViewNode.fullPath}
+        style={{
+          paddingLeft: `${level * 20}px`,
+          border: '1px solid white ',
+          borderRadius: '8px'
+        }} // 20px per level
+      >
+        <div style={{ paddingLeft: '20px', display: 'flex', alignItems: 'center' }}>
+          <InsertDriveFileIcon sx={{ color: '#4da6ff' }}></InsertDriveFileIcon>
+          <span>{fileSystemViewNode.name}</span>
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div
+        key={fileSystemViewNode.fullPath}
+        style={{
+          paddingLeft: `${level * 20}px`,
+          border: '1px solid white ',
+          borderRadius: '8px'
+        }} // 20px per level
+      >
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <KeyboardArrowDownSharpIcon />
+          <FolderIcon sx={{ color: yellow[500] }}></FolderIcon>
+          <span>{fileSystemViewNode?.name}</span>
+        </div>
+        {fileSystemViewNode.groupedChildren.map((group, index) => {
+          return (
+            <div key={group[0].fullPath + ' - group'}>
+              {group.map((f) => {
+                return getTreeNode(f, level + 1)
+              })}
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+}
+
 const TreeView = forwardRef<HTMLDivElement, TreeViewProps>(({ isLeft, ...rest }, ref) => {
   const fileSystemNode1 = useSelector(selectFileSystemNode1)
   const fileSystemNode2 = useSelector(selectFileSystemNode2)
@@ -73,7 +124,7 @@ const TreeView = forwardRef<HTMLDivElement, TreeViewProps>(({ isLeft, ...rest },
     const fileSystemViewNode = getFileSystemViewNode(fileSystemNode)
     return (
       <Box ref={ref} {...rest}>
-        <div className="treeViewAfterSelectionContainer"></div>
+        <div className="treeViewAfterSelectionContainer">{getTreeNode(fileSystemViewNode, 0)}</div>
       </Box>
     )
   }
